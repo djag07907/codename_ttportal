60302635754b46aa8c645072fefe50bf38604dde
Daniel Alvarez <daniel.alvarez@nucodiga.com>
2025-02-13 07:28:30 -0600

    routes implementation

--- lib/routes/landing_constants.dart
+++ lib/routes/landing_constants.dart
@@ -1,3 +1,8 @@
 const String rootRoute = '/';
 const String loginRoute = '/login';
 const String adminPanelRoute = '/admin';
+const String regularUserRoute = '/user';
+const String usersRoute = '/users';
+const String companiesRoute = '/comoanies';
+const String dashboardsRoute = '/dashboards';
+const String licensesRoute = '/licenses';
--- lib/routes/landing_routes.dart
+++ lib/routes/landing_routes.dart
@@ -1,4 +1,10 @@
+import 'package:codename_ttportal/admin/admin_screen.dart';
+import 'package:codename_ttportal/companies/companies_screen.dart';
+import 'package:codename_ttportal/dashboard/dashboard_screen.dart';
+import 'package:codename_ttportal/home/home_screen.dart';
+import 'package:codename_ttportal/licenses/licenses_screen.dart';
 import 'package:codename_ttportal/login/login_screen.dart';
+import 'package:codename_ttportal/user/user_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'landing_constants.dart';
@@ -15,6 +21,22 @@ class LandingRoutes {
       case loginRoute:
         return MaterialPageRoute(
           builder: (_) => const LoginScreen(),
+        );
+      case usersRoute:
+        return MaterialPageRoute(
+          builder: (_) => const UserScreen(),
+        );
+      case companiesRoute:
+        return MaterialPageRoute(
+          builder: (_) => const CompaniesScreen(),
+        );
+      case licensesRoute:
+        return MaterialPageRoute(
+          builder: (_) => const LicensesScreen(),
+        );
+      case dashboardsRoute:
+        return MaterialPageRoute(
+          builder: (_) => const DashboardScreen(),
         );
       default:
         return MaterialPageRoute(
